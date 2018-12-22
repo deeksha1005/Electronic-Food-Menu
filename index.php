633bec21a62e662fc7f3ce9639470e1ed96aa656
Deeksha Sisodiya <dsisodiya.105@gmail.com>
2018-12-22 13:40:12 +0530

    On branch master

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> Home-1 </title>
+	<link rel="stylesheet"  type="text/css"  href="stylesheet1.css">
+</head>
+
+<body>
+
+  <header>
+		<div class="container">
+			  <div id ="branding">
+
+		  <nav>
+			  <ul>
+				   <!--<li><a href="register.php"> Login Panel </a></li> -->
+		       <li><a href="admin_panel.php"> Admin </a></li>
+					 <li><a href="services.php"> Services </a></li>
+				 <li><a href="records.php"> Records </a></li>
+				 <li><a href="customer/customer_view.php"> Customer </a></li>
+			  </ul>
+		  </nav>
+
+		 	<div class = "logo">
+			  <img src="logo3.png">
+			</div>
+			<div class ="heading">
+				<p> Coz heart is where food is.. </p>
+				<h1> FOOD-TABLE </h1>
+			</div>
+	  </div>
+  </div>
+</header>
+<div id="content">
+	<section id ="boxes">
+		<div class ="container">
+			<h2> About </h2>
+			<p> Class and elegance provide an inviting ambiance that any dinner must
+				 experience for those involved to come back for more.
+				 Class and elegance provide an inviting ambiance that any dinner must
+	 				 experience for those involved to come back for more.<br>
+					 Class and elegance provide an inviting ambiance that any dinner must
+		 				 experience for those involved to come back for more.
+		 				 Class and elegance provide an inviting ambiance that any dinner must
+		 	 				 experience for those involved to come back for more.
+			</p>
+		</div>
+	</section>
+
+	<aside id= "sidebar">
+		<div class="mumbai">
+
+		</div>
+	</aside>
+
+</div>
+<div id="footer">
+		<p align="center" > FOOD-TABLE  E-menu, Copyright &copy; 2018 </p>
+</div>
+
+</body>
+
+</html>
+
+<?php
+?>
