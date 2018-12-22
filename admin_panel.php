035495d768c6f1a4a4797cc73c5c699fdd860038
Deeksha Sisodiya <dsisodiya.105@gmail.com>
2018-12-22 13:39:25 +0530

    On branch master

---
+++ admin_panel.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1 , shrink-to-fit=no">
+
+    <title>Admin Panel-3</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+    span{
+      color:#e8491d;
+    }
+    .fixed-bottom{
+      background-color: #e8491d;
+    }
+    a{
+      color:white;
+    }
+    a:hover{
+      color:black;
+      font-weight:lighter;
+      text-decoration: none;
+    }
+    </style>
+
+  </head>
+  <body>
+
+    <div class="navbar navbar-expand-sm bg-dark navbar-light ">
+      <style>
+        .navbar{
+          border-bottom: 3px solid #e8491d;
+        }
+      </style>
+      <h4 class="text-white">FOOD-TABLE</h4>
+      <ul class="navbar-nav ml-auto">
+        <!--<li class="nav-item">
+        <a href="http://localhost/e-menu/" class="navbar-brand d-flex align-items-center text-white">
+          <strong><span> Home </span></strong>
+        </a>
+        </li>-->
+        <li><a class="nav-link text-white" href="menu/category.php">Menu &nbsp</a></li>
+        <li><a class="nav-link text-white" href="#">Cashier &nbsp</a></li>
+        <li><a class="nav-link text-white" href="#">Records  &nbsp</a></li>
+        <li><a class="nav-link text-white" href="#">Logout</a></li>
+      </ul>
+    </div>
+
+  <div class="container text-center">
+    <h1> WELCOME </h1>
+
+
+  </div>
+
+    <footer class="fixed-bottom">
+        <p class="text-right"><a href="more_food.php">Add more types? &nbsp</a></p>
+    	  <p class="navbar-brand ml-auto text-white">
+          <a href="http://localhost/e-menu/"><strong>&nbsp FOOD-TABLE</strong></a> E-menu, Copyright &copy 2018
+        </p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+
+  </body>
+</html>
